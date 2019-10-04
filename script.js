f3ba4b398753c0eb196e6363732f276a6d542f7a
Alen <Alenslo@gmx.net>
2019-10-05 00:32:14 +0200

    random dices are shown after button has been clicked

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+hideDice(); // hide dice
 
- 
-function hideDice() {
+
+function hideDice() { // Dices are not shown on the page
     document.querySelectorAll("svg")[0].style.display='none'
     document.querySelectorAll("svg")[1].style.display= 'none';
     document.querySelectorAll("svg")[2].style.display= 'none';
+    document.querySelectorAll("svg")[3].style.display= 'none';
+    document.querySelectorAll("svg")[4].style.display= 'none';
+    document.querySelectorAll("svg")[5].style.display= 'none';
 }
 
-hideDice();
- 
-let randomNumber = Math.round(Math.random()*2)+1;
-
-console.log(randomNumber)
-
-showResult(randomNumber);
-
-// Show respective dice
-function showResult(input) {
-
-    switch(input) {
+function showResult() { // Create and display the random dice number
+    hideDice();
+    randomNumber = Math.round(Math.random()*5)+1; 
+    console.log(randomNumber)
+    
+    switch(randomNumber) {
         case 1: document.querySelector("#one").style.display= 'block';
         break;
         case 2: document.querySelector("#two").style.display= 'block';
         break;
         case 3: document.querySelector("#three").style.display= 'block';
         break;
+        case 4: document.querySelector("#four").style.display= 'block';
+        break;
+        case 5: document.querySelector("#five").style.display= 'block';
+        break;
+        case 6: document.querySelector("#six").style.display= 'block';
+        break;
     }
+    
 }
 
 

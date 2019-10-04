7a0721d49c7e5a60c39e9982acf16282dcad1c93
Alen <Alenslo@gmx.net>
2019-10-05 01:04:52 +0200

    playerWin() function and some refractoring

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 hideDice(); // hide dice
-
+score = 0;
 
 function hideDice() { // Dices are not shown on the page
-    document.querySelectorAll("svg")[0].style.display='none'
-    document.querySelectorAll("svg")[1].style.display= 'none';
-    document.querySelectorAll("svg")[2].style.display= 'none';
-    document.querySelectorAll("svg")[3].style.display= 'none';
-    document.querySelectorAll("svg")[4].style.display= 'none';
-    document.querySelectorAll("svg")[5].style.display= 'none';
+    let svg = document.querySelectorAll('svg');
+        svg.forEach((entries) => {
+        entries.style.display = 'none';
+    })
 }
 
 function showResult() { // Create and display the random dice number
@@ -15,23 +13,36 @@ function showResult() { // Create and display the random dice number
     randomNumber = Math.round(Math.random()*5)+1; 
     console.log(randomNumber)
     
+    
     switch(randomNumber) {
         case 1: document.querySelector("#one").style.display= 'block';
+        playerWin();
         break;
         case 2: document.querySelector("#two").style.display= 'block';
+        playerWin();
         break;
         case 3: document.querySelector("#three").style.display= 'block';
+        playerWin();
         break;
         case 4: document.querySelector("#four").style.display= 'block';
+        playerWin();
         break;
         case 5: document.querySelector("#five").style.display= 'block';
+        playerWin();
         break;
         case 6: document.querySelector("#six").style.display= 'block';
-        break;
+        
     }
     
 }
 
+function playerWin() {
+    if(document.querySelectorAll('input')[0].value == randomNumber) {
+        alert('You have Won')
+        score +=1;
+        console.log('score' + score)
+    }
+}
 
 
 
